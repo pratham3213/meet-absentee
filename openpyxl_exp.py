0581e2ebf6085efe85b866fc26d3c6559edfdbff
pratham3213 <77375030+pratham3213@users.noreply.github.com>
2021-07-05 23:31:02 +0530

    Add files via upload

---
+++ openpyxl_exp.py
@@ -0,0 +1,59 @@
+from os import *
+from tkinter import *
+import datetime
+import openpyxl as xl
+import pandas as pd
+from tkinter import filedialog
+
+
+path=getcwd()
+
+def tkinter_window():
+	root=Tk()
+	root.title("Choose Your CSV File")
+	root.filename=filedialog.askopenfilename(initialdir=path, title="Select A File")
+	return root.filename
+'''def file1():
+	l = []
+	f = open("/home/sam/Desktop/experimentation/story.txt", "r")
+	for i in f:
+		l.append(i)
+
+	print(l)
+'''
+
+def csv():
+	data = pd.read_csv(tkinter_window())
+
+	names=data["Participants"].tolist()
+	return names
+
+def logic():
+	pass
+
+def excel():
+
+	wb = xl.load_workbook("12_B.xlsx")
+
+	sheet = wb["Sheet1"]
+
+	Present_Student=csv()
+
+	for row in range(2, sheet.max_row+1):
+		cell1 = sheet[f"B{row}"]
+		date = sheet[f"C{1}"]
+		date.value=datetime.date.today()
+		cell2=sheet[f"C{row}"]
+		
+		for present in Present_Student:
+			if present==cell1.value:
+				cell2.value="Present"
+				
+	
+
+	wb.save("11_B.xlsx")
+
+
+x=csv()
+for i in x:
+	print(i)
---
+++ requirements.py
@@ -0,0 +1,5 @@
+import subprocess as sub
+
+sub.run(["pip3","install","tk"])
+sub.run(["pip3", "install", "openpyxl"])
+sub.run(["pip3", "install", "pandas"])
